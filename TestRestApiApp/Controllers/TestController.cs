407d63e9acdd510a9b1fb6bb0e4705ef886dca5d
dharminder <dharminder@cybrain.co.in>
2021-01-12 11:48:26 +0530

    Add project files.

---
+++ TestRestApiApp/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestRestApiApp.Models;
+
+namespace TestRestApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ConnectionStrings connectionStrings;
+        public TestController(IOptions<ConnectionStrings> connectionStrings)
+        {
+            this.connectionStrings = connectionStrings.Value;
+        }
+    }
+}
